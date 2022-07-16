029eecac379e41f010b179e496b7f01f066336cf
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-07-16 22:44:51 +0300

    CustomMiddleware

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const pageController = require('../controllers/pageController');
+const redirectMiddleware = require('../middlewares/redirectMiddleware')
 
 const router = express.Router();
 
 router.route('/').get(pageController.getIndexPage);// http://localhost/
 router.route('/about').get(pageController.getAboutPage);// http://localhost/about
-router.route('/register').get(pageController.getRegisterPage);// http://localhost/register
-router.route('/login').get(pageController.getLoginPage);// http://localhost/login
+router.route('/register').get(redirectMiddleware, pageController.getRegisterPage);// http://localhost/register
+router.route('/login').get(redirectMiddleware, pageController.getLoginPage);// http://localhost/login
 
-module.exports = router;
+module.exports = router;     
