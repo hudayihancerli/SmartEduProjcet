b0cf1f5e9ba0889b170de2bd8ab5d75f790d2c5d
Mahmut hüdayi hançerli <m.hudayihancerli@gmail.com>
2022-07-18 23:22:24 +0300

    SearchArea

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -24,6 +24,7 @@ exports.getAllCourse = async (req, res) => {
     try {
 
         const categorySlug = req.query.categories;
+        const query = req.query.search;
         const category = await Category.findOne({ slug: categorySlug })
 
         let filter = {}
@@ -32,7 +33,24 @@ exports.getAllCourse = async (req, res) => {
             filter = { category: category._id }
         }
 
-        const courses = await Course.find(filter).sort('-createdAt').populate('user');
+        if(query){
+            filter = {
+                name : query
+            }
+        }
+
+        if(!query && !categorySlug){
+            filter.name = "",
+            filter.category = null
+        }
+
+
+        const courses = await Course.find({
+            $or:[
+                {name:{$regex: '.*' + filter.name + '.*', $options:'i'}},
+                {category:filter.category}
+            ]
+        }).sort('-createdAt').populate('user');
         const categories = await Category.find();
 
         res.status(200).render('courses', {
